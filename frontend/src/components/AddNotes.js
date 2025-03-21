9ce9ee2d1aa55cfb61709abc9b2f2ad88841c86f
Tadeus Vito <160498521+tadeusvito@users.noreply.github.com>
2025-03-21 23:23:09 +0700

    Update AddNotes.js

--- frontend/src/components/AddNotes.js
+++ frontend/src/components/AddNotes.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from "../utils";
 
 const AddNotes = () => {
     const [title, setTitle] = useState("");
@@ -10,7 +11,7 @@ const AddNotes = () => {
     const saveNote = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/note', {
+            await axios.post(`${BASE_URL}/note`, {
                 title,
                 content
             });
